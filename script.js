4b312ae576c5338e3457cbd4f6813ac0dd8f3ab2
orzcode <71000311+orzcode@users.noreply.github.com>
2022-12-20 18:21:58 +1300

    JS mouse-over - but not working

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
+const grid = document.getElementById("grid");
+
 const div = document.createElement("div");
 div.classList.add('gridBlock');
 for (let i = 16; i > 0; i--){
 	grid.append(div.cloneNode(true));
+	div.addEventListener('mouseenter',(event) => {
+		document.querySelector(".gridBlock").style.backgroundColor = "lightblue";
+	});
 };
+
