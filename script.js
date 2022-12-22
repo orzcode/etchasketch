23eb4284a5dd7a03ef591ae0cb3bd7cbdf3ce12f
orzcode <71000311+orzcode@users.noreply.github.com>
2022-12-22 17:28:47 +1300

    resize function finalized, now to add buttons

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+let newSize;
 const div = document.createElement("div");
 //Create a block for the grid
 
-for (let i = 16; i > 0; i--){
+for (let i = 100; i > 0; i--){
 	div.classList.add('gridBlock');
 	grid.append(div.cloneNode(true));	
 };
@@ -19,7 +20,7 @@ document.querySelectorAll(".gridBlock").forEach(item => {
 //this part is cumbersome due to the 'fake' DOM gridblocks.
 
 function gridSizer(){
-	let newSize = prompt("How many squares per side for new grid?", "Example: 4");
+	newSize = prompt("How many squares per side for new grid?", "Example: 4");
 	if (newSize){
 	if (isNaN(newSize) || (newSize < 4) || (newSize > 100)){
 		alert("Sorry - enter a NUMBER, and keep it between 4 ~ 100");
@@ -30,3 +31,15 @@ function gridSizer(){
 	else return	
 };
 
+
+function resize(num){
+	alert("newsize is " + num);
+	grid.style.gridTemplateRows = "repeat(" + num + ", 1fr)";
+	grid.style.gridTemplateColumns = "repeat(" + num + ", 1fr)";
+	document.querySelectorAll(".gridBlock").forEach(item => {
+		item.style.height = num + "rem";
+		item.style.width = num + "rem";
+	});
+}
+//Proof of concept for the resize function. Uses a variable (newSize) which
+//must be invoked (i.e. this func must be invoked) at end of clicking button func
