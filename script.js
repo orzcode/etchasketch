fe2f55c2228305357dd5a3896eff11750e839502
orzcode <71000311+orzcode@users.noreply.github.com>
2022-12-26 15:27:13 +1300

    fix resize and mouseover func

--- script.js
+++ script.js
@@ -1,67 +1,29 @@
 gridSize(16);
 //Run the INITIAL gridsize function (4x4)
 
-function gridSize(sizeSquared){
-	const div = document.createElement("div");
+function gridSize(sizeSquared){	
 for (let i = sizeSquared; i > 0; i--){
-	newColorChange();
+	const div = document.createElement("div");
 	div.classList.add('gridBlock');
-	grid.append(div.cloneNode(true));
-};
+	div.addEventListener('mouseenter', function(){
+		div.style.backgroundColor = "lightblue";
+	})
+	grid.append(div);
+	};
 }
-//Create a block for the grid,
-//Add the class .gridBlock then clone the div & append to the grid, x times based on resize(func)
+//Create a div cell for the grid,
+//Add the class .gridBlock, append to the grid, apply mouseover event
+//loops x times based on resize(func)
 
-function removeGridBlocks(){
-	document.querySelectorAll(".gridBlock").forEach(item => {
-	item.remove();
-})}
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 //Removes all child nodes, used during grid re-sizing
 
-//---------------------------------------------------
-// function changeGridBlockColor(){
-// 	document.querySelectorAll(".gridBlock").forEach(item => {
-// 	item.addEventListener('mouseenter', event => {
-// 		item.style.backgroundColor = "lightblue";
-// 	})
-// })};
-//Go through and apply colorchange on mouseenter, to each one
-//THIS IS CAUSING MAJOR SLOWDOWNS ABOVE 16X16
-//------------------------------------------------------------
-
-//CURRENTLY NOT WORKING
-function newColorChange(){
-	grid.addEventListener('mouseenter', function (event) {
-
-		if (event.target.classList.contains('.gridBlock')) {
-		event.target.style.backgroundColor = "lightblue";
-	 }	//else {alert("newColorChange function isn't working!")};//
-})
-};
-//CURRENTLY NOT WORKING
-
-
-
-
-//----DEPRECATED - alert asking for grid size - now we use buttons
-// function gridSizer(){
-// 	newSize = prompt("How many squares per side for new grid?", "Example: 4");
-// 	if (newSize){
-// 	if (isNaN(newSize) || (newSize < 4) || (newSize > 100)){
-// 		alert("Sorry - enter a NUMBER, and keep it between 4 ~ 100");
-// 		gridSizer();
-// 	   }
-// 	else {return newSize};
-// 	}
-// 	else return	
-// };
-//----DEPRECATED - alert asking for grid size - now we use buttons
-
-
-
 function resize(num){
-	// alert("resize is " + num);
-	removeGridBlocks();
+	removeAllChildNodes(grid);
 	gridSize(num * num);
 	grid.style.gridTemplateRows = "repeat(" + num + ", 1fr)";
 	grid.style.gridTemplateColumns = "repeat(" + num + ", 1fr)";
@@ -70,9 +32,7 @@ function resize(num){
 		item.style.width = (100 / num) + "rem";
 	});
 }
-//Resize function. Uses a variable (newSize) which must be
-//invoked (i.e. this func must be invoked) upon clicking button.
-//Takes the btn value as param.
+//Takes the resize btn value as param.
 //Also runs the gridBlock element remover FIRST.
 //the ORDER of this function is vital else grid won't render properly.
 
@@ -88,3 +48,8 @@ function toggleGrid(){
 		document.querySelectorAll(".gridBlock").forEach(item => {
 		item.style.outline == "1px solid black";
 		})}};
+//Toggles grid lines - in the form of css outline
+
+function clear(){
+	//resets grid colors to initial
+}
